1b5dda287b2e975ce00dad8752e6cc889070daa5
tothadam98 <75333311+tothadam98@users.noreply.github.com>
2021-10-18 12:34:38 +0300

    Labor2->1.feladat

---
+++ src/main/kotlin/IDictionary.kt
@@ -0,0 +1,5 @@
+interface IDictionary {
+    fun add(word: String): Boolean;
+    fun find(word: String): Boolean;
+    fun size(): Int;
+}
---
+++ src/main/kotlin/ListDictionary.kt
@@ -0,0 +1,32 @@
+object ListDictionary : IDictionary {
+    private val words = mutableListOf<String>()
+    private val name = "English Dictionary"
+
+    fun getName ():String{
+        return this.name;
+    }
+
+    override fun add(word: String): Boolean {
+        return words.add(word)
+    }
+
+    override fun find(word: String): Boolean {
+       var index = -1
+        index = words.indexOf(word)
+        return index >= 0
+    }
+
+    override fun size(): Int {
+        return words.size
+    }
+}
+
+fun main(args:Array<String>){
+    println(ListDictionary.getName())
+    ListDictionary.add("Kecske")
+    ListDictionary.add("Macska")
+    ListDictionary.add("Kutya")
+    println(ListDictionary.find("Kecske"))
+    println(ListDictionary.size())
+
+}
