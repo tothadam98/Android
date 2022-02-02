5afe4a66d53e4aabd5a95e681a9a539c11be4373
tothadam98 <75333311+tothadam98@users.noreply.github.com>
2022-02-02 23:24:56 +0300

    rtf

---
+++ app/app/src/main/java/com/example/marketplace/api/RetorfitInstance.kt
@@ -0,0 +1,24 @@
+package com.example.marketplace.api
+
+import com.example.marketplace.utils.Constants.BASE_URL
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+object RetrofitInstance {
+    private val moshi = Moshi.Builder()
+        .add(KotlinJsonAdapterFactory())
+        .build()
+
+    val retrofit = Retrofit.Builder()
+        .addConverterFactory(MoshiConverterFactory.create(moshi))
+        .baseUrl(BASE_URL)
+        .build()
+
+    val api: MarketApi by lazy{
+        retrofit.create(MarketApi :: class.java)
+    }
+
+
+}
